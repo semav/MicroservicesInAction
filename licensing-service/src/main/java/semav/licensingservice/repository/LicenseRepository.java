d120c2eda2606bb078898232451ec6e407a968cd
semav <1semav@gmail.com>
2018-12-21 12:19:00 +0300

    test

--- licensing-service/src/main/java/semav/licensingservice/repository/LicenseRepository.java
+++ licensing-service/src/main/java/semav/licensingservice/repository/LicenseRepository.java
@@ -8,6 +8,5 @@ import java.util.List;
 public interface LicenseRepository extends CrudRepository<License, String> {
     List<License> findByOrganizationId(String organizationId);
     License findByOrganizationIdAndLicenseId(String organizationId, String licenseId);
-
 }
 
